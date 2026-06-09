9680ab4c98c1ad0d80e9442e22f550776cf4cc20
Júlia Lemos Souza <jucalemossouza@gmail.com>
2026-06-09 08:29:40 -0300

    ajustes

--- condicoes_operacionais.py
+++ condicoes_operacionais.py
@@ -35,6 +35,25 @@ print("\nALERTAS\n")
 if temperatura > 30:
     print("ALERTA: Temperatura da água elevada.")
 
+# Energia
+
+energia_solar = random.randint(0, 100)
+energia_eolica = random.randint(0, 100)
+consumo_energia = random.randint(50, 120)
+
+print("Produção Solar:", energia_solar, "%")
+print("Produção Eólica:", energia_eolica, "%")
+print("Consumo Energético:", consumo_energia, "%")
+
+if energia_solar < 30:
+    print("ALERTA: Baixa geração de energia solar.")
+
+if energia_eolica < 20:
+    print("ALERTA: Baixa geração de energia eólica.")
+
+if consumo_energia > 100:
+    print("ALERTA: Consumo energético elevado.")
+
 # Maré
 
 if mare > 6:
@@ -56,7 +75,7 @@ if plastico == "Alto":
 if emissao == "Sim":
     print("ALERTA: Possível emissão ilegal identificada.")
 
-print("\nANÁLISE DA IA\n")
+print("\nCENTRO DE MONITORAMENTO\n")
 
 if oleo == "Sim":
     print("Recomendação: Acionar equipe de contenção do petróleo.")
@@ -73,14 +92,18 @@ elif plastico == "Alto":
 else:
     print("Recomendação: Situação estável.")
 
-    fase_lua = random.choice([
-        "Lua Nova",
-        "Lua Crescente",
-        "Lua Cheia",
-        "Lua Minguante"
-    ])
+# Lua
 
-    print("Fase da Lua:", fase_lua)
+fase_lua = random.choice([
+    "Lua Nova",
+    "Lua Crescente",
+    "Lua Cheia",
+    "Lua Minguante"
+])
 
-    if fase_lua == "Lua Cheia" and mare > 6:
-        print("A Lua Cheia pode estar influenciando a elevação da maré.")
+print("\nINFORMAÇÕES COMPLEMENTARES")
+print("Fase da Lua:", fase_lua)
+
+if fase_lua == "Lua Cheia" and mare > 6: 
+    print("A Lua Cheia pode estar influenciando a elevação da maré.") 
+print("\nMonitoramento finalizado.")
